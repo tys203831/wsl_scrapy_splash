4ac28a3604a45e1bea27ff3eba6250aebe689f0c
tys203831 <tys203831@gmail.com>
2021-10-23 15:03:22 +0800

    init

--- steam_store/steam_store/items.py
+++ steam_store/steam_store/items.py
@@ -2,19 +2,80 @@
 #
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
+# https://docs.scrapy.org/en/latest/topics/loaders.html#input-and-output-processors
 
 import scrapy
+from itemloaders.processors import Identity, TakeFirst, MapCompose, Join
+from w3lib.html import remove_tags
+from scrapy.selector import Selector
 
+def get_platforms(one_class): # since items.py does not take list as argument, it takes single value inside the list in loop 
+    platforms = []
+    platform = one_class.split(' ')[-1]
+    if "win" in platform:
+        platforms.append("Windows")
+    if "mac" in platform:
+        platforms.append("Mac os")
+    if "linux" in platform:
+        platforms.append("Linux")
+    if "vr_supported" in platform:
+        platforms.append("VR Supported")
+    return platforms
+
+def get_original_price(html_markup):
+    original_price = ""
+    selector_obj = Selector(text=html_markup)
+    div_with_discount = selector_obj.xpath(".//div[@class='col search_discount responsive_secondrow']/span/text()")
+    original_price = ""
+    if len(div_with_discount) > 0:
+        original_price = str(selector_obj.xpath(".//div[contains(@class,'search_price discounted')]/span/strike/text()").get()).strip()  
+    else:
+        original_price = str(selector_obj.xpath(".//div[@class='col search_price  responsive_secondrow']/text()").get()).strip()
+    return original_price
+
+def clean_discounted_price(discounted_price):
+    if discounted_price:
+        return discounted_price.strip()
+    return discounted_price
+
+def clean_discount_rate(discount_rate):
+    if discount_rate:
+        discount_rate = discount_rate.lstrip("-")
+        return discount_rate
+    return discount_rate
 
 class SteamStoreItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    game_url = scrapy.Field()
-    img_url = scrapy.Field()
-    game_name = scrapy.Field()
-    release_date = scrapy.Field()
-    platform = scrapy.Field()
-    rating = scrapy.Field()
-    original_price = scrapy.Field()
-    discounted_price = scrapy.Field()
-    discount_rate = scrapy.Field()
+    game_url = scrapy.Field(
+        output_processor = TakeFirst()
+    )
+    img_url = scrapy.Field(
+        output_processor = TakeFirst()
+    )
+    game_name = scrapy.Field(
+        output_processor = TakeFirst()
+    )
+    release_date = scrapy.Field(
+        output_processor = TakeFirst()
+    )
+    platform = scrapy.Field(
+        input_processor = MapCompose(get_platforms),
+        output_processor = Identity() 
+    )
+    rating = scrapy.Field(
+        input_processor = MapCompose(remove_tags),
+        output_processor = TakeFirst()
+    )
+    original_price = scrapy.Field(
+        input_processor = MapCompose(get_original_price, str.strip),
+        output_processor = Join("")
+    )
+    discounted_price = scrapy.Field(
+        input_processor = MapCompose(clean_discounted_price),
+        output_processor = TakeFirst()
+    )
+    discount_rate = scrapy.Field(
+        input_processor = MapCompose(clean_discount_rate),
+        output_processor = TakeFirst()
+    )
--- steam_store/steam_store/spiders/best_selling.py
+++ steam_store/steam_store/spiders/best_selling.py
@@ -1,54 +1,37 @@
 import scrapy
 from ..items import SteamStoreItem
-from w3lib.html import remove_tags
+from scrapy.loader import ItemLoader
 
 class BestSellingSpider(scrapy.Spider):
     name = 'best_selling'
     allowed_domains = ['store.steampowered.com']
     start_urls = ['http://store.steampowered.com/search/?filter=topsellers/']
 
-    def get_platforms(self, multiple_classes):
-        platforms = []
-        platform = [one_class.split(' ')[-1] for one_class in multiple_classes]
-        if "win" in platform:
-            platforms.append("Windows")
-        if "mac" in platform:
-            platforms.append("Mac os")
-        if "linux" in platform:
-            platforms.append("Linux")
-        if "vr_supported" in platform:
-            platforms.append("VR Supported")
-        return platforms
-
-    def clean_discount_rate(self, discount_rate):
-        if discount_rate:
-            discount_rate = discount_rate.lstrip("-")
-        return discount_rate
 
     def get_original_price(self, selector_obj):
         div_with_discount = selector_obj.xpath(".//div[@class='col search_discount responsive_secondrow']/span/text()")
         original_price = ""
         if len(div_with_discount) > 0:
-            original_price = str(selector_obj.xpath(".//div[contains(@class,'search_price discounted')]/span/strike/text()").get()).strip()
+            original_price = selector_obj.xpath(".//div[contains(@class,'search_price discounted')]/span/strike/text()").get()
             
         else:
-            original_price = str(selector_obj.xpath(".//div[@class='col search_price  responsive_secondrow']/text()").get()).strip()
+            original_price = selector_obj.xpath(".//div[@class='col search_price  responsive_secondrow']/text()").get() # why getall()
         return original_price
 
     def parse(self, response):
-        steam_item = SteamStoreItem()
         games = response.xpath("//div[@id='search_resultsRows']/a")
         for game in games: 
-            steam_item["game_url"] = game.xpath("./@href").get()
-            steam_item ["img_url"] = game.xpath(".//img/@src").get()
-            steam_item ["game_name"] = game.xpath(".//span[@class='title']/text()").get()
-            steam_item ["release_date"] = game.xpath(".//div[@class='col search_released responsive_secondrow']/text()").get()
-            steam_item ["platform"] = self.get_platforms(game.xpath(".//span[contains(@class, 'platform_img')]/@class | .//span[@class='vr_supported']/text()").getall())
-            steam_item ["rating"] = remove_tags(game.xpath(".//span[contains(@class, 'search_review_summary')]/@data-tooltip-html").get())
-            steam_item ["original_price"] = self.get_original_price(game)
-            steam_item ["discount_rate"] = self.clean_discount_rate(game.xpath(".//div[contains(@class,'search_discount')]/span/text()").get())
-            steam_item ["discounted_price"] = [discount_price.strip() for discount_price in game.xpath(".//div[@class='col search_price discounted responsive_secondrow']/text()").getall() if discount_price.strip() != ""]
-            yield steam_item
+            loader = ItemLoader(item = SteamStoreItem(), selector=game)
+            loader.add_xpath("game_url", "./@href")
+            loader.add_xpath("img_url", ".//img/@src")
+            loader.add_xpath("game_name", ".//span[@class='title']/text()")
+            loader.add_xpath("release_date", ".//div[@class='col search_released responsive_secondrow']/text()")
+            loader.add_xpath("platform", ".//span[contains(@class, 'platform_img')]/@class | .//span[@class='vr_supported']/text()") # getall()
+            loader.add_xpath("rating",  ".//span[contains(@class, 'search_review_summary')]/@data-tooltip-html")
+            loader.add_xpath("original_price", ".//div[contains(@class, 'search_price_discount_combined' )]")
+            loader.add_xpath("discount_rate", ".//div[contains(@class,'search_discount')]/span/text()")
+            loader.add_xpath("discounted_price", ".//div[@class='col search_price discounted responsive_secondrow']/text()")
+            yield loader.load_item()
 
         abs_url = response.xpath("//a[@class='pagebtn' and text()='>']/@href").get()
         yield scrapy.Request(url = abs_url, callback=self.parse)
